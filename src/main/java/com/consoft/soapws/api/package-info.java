7d46660595e656e78db3b9847f19892a518ad748
Davide Giancane <davide.giancane@consoft.it>
2020-07-10 12:26:20 +0200

    getAllCustomers funzionante

--- src/main/java/com/consoft/soapws/api/package-info.java
+++ src/main/java/com/consoft/soapws/api/package-info.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.3.2 
 // Vedere <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2020.07.10 alle 11:36:11 AM CEST 
+// Generato il: 2020.07.10 alle 12:10:05 PM CEST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.consoft.com/soapws/api", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
